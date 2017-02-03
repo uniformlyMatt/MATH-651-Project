f1d7c8341ab3a40fee6b38d3c7b41252a5c25289
snimpids <snimpids@gmail.com>
2017-02-03 15:10:20 -0700

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,3 +1,26 @@
 import numpy as np
 
-def Learn(TrainingSet, Class, Epoch, LearningRate):
+def Sgn(y):
+  if y > 0:
+    return 1
+  else:
+    return -1
+
+def Learn(TrainingSet, Class, Epochs, LearningRate):
+  n, m = TrainingSet.shape()
+  
+  # Defining random weights
+  w = 0.5*np.random.rand(1,m)
+  a = LearningRate
+  
+  for i in range(0, Epochs):
+    for j in range(0, n):
+      x = TrainingSet[j,:]
+      wsum = np.dot(w,x)
+      
+      y = Sgn(wsum)
+      Error = Class(j) - y
+      
+      w += Error*a*x
+  
+  return w
