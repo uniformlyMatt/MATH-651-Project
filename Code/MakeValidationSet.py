cbfdeb74599a616df5fa04666020f051ab0ec6b9
snimpids <snimpids@gmail.com>
2017-04-08 17:42:12 -0600

    Update MakeValidationSet.py

--- Code/MakeValidationSet.py
+++ Code/MakeValidationSet.py
@@ -1,1 +1,10 @@
 
+
+''' This script takes in a grayscale image, extracts all objects containing 
+more than a specified number of pixels, and saves each object as an 
+individual .png file. '''
+
+import numpy as np
+from scipy import ndimage
+from PIL import Image
+import matplotlib.pyplot as plt
